0dfc2acf60e02299f196332f5b3763df82b6ea1e
theweiyu <87156384+theweiyu@users.noreply.github.com>
2021-07-09 01:25:13 +0100

    setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='lpsolvewrapper',
     version='1.0.0',
-	author='Weiyu Chen',
+    author='Weiyu Chen',
     packages=['lpsolvewrapper'],
     package_data={'lpsolvewrapper': ['liblpsolve55.a', 'liblpsolve55.so', 'lpsolve55.cpython-36m-x86_64-linux-gnu.so', 'lpsolve55-5.5.0.9-py3.6.egg-info']}
 )
