8017cc9277e68abe099e40d7e3fc75f4306853c5
theweiyu <87156384+theweiyu@users.noreply.github.com>
2021-07-12 13:03:38 +0100

    update for 1.0.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='lpsolvewrapper',
-    version='1.0.0',
+    version='1.0.1',
     author='Weiyu Chen',
     keywords='lpsolve python3 wrapper compiled',
     packages=['lpsolvewrapper'],
